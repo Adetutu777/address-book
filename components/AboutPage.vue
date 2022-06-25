17da61fdbcae888c458d10f3bfe86f7ccc7644b0
Adetutu <adetutugbangbola@gmail.com>
2022-06-25 11:04:05 +0100

    update

---
+++ components/AboutPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-for="todos in todo" :key="todos.id">
+    {{todos.title}}
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import {  ref, useFetch } from '@nuxtjs/composition-api';
+    export default {
+        setup(){
+            const todo = ref('')
+             useFetch(async () => {
+      await axios.get('https://jsonplaceholder.typicode.com/todos')
+     .then( response => todo.value = response?.data)
+      .catch(error => console.log(error))
+    })
+
+            return { todo}
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
-        <p>This is an about me section</p>
+    <button @click="showData">click</button>
+    <div v-if="show">
+    <!-- <div v-for="data in dataList" :key="data">
+    {{data}} -->
+      <LazyAboutPage  />
+      </div>
     </div>
+    <!-- </div> -->
 </template>
 
 <script>
+import { ref, onMounted } from '@vue/composition-api'
     export default {
-        
+        setup(){
+            const show =ref(false)
+            const showData=()=>{
+                show.value=true
+            }
+
+            // const dataList = new Array(200).fill('hello world')
+            return { show, showData,}
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
